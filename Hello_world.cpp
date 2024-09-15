f9e932569899c1b023d30652a4d46247790594cc
Mazer1x <tetuhin@inbox.ru>
2024-09-15 13:54:09 +0300

    Simple code written

---
+++ Hello_world.cpp
@@ -0,0 +1,10 @@
+/* Maxim Tetuchin tetuhin@inbox.ru | st128993@student.spbu.ru
+Hello_world project
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello world";
+    return 0;
+}
