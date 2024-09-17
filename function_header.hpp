936a4750ec438421b506a89b09a0e3f59217cb6b
ShadeOfMorr <shcheka.d@gmail.com>
2024-09-17 13:49:10 +0300

    Update function_header.hpp

--- function_header.hpp
+++ function_header.hpp
@@ -1,5 +1,8 @@
 /* Maxim Tetuchin tetuhin@inbox.ru | st128993@student.spbu.ru
 Hello_world project
 */
+/* Эта директива удобная, согласен. Но на самом деле ее даже в стандарте языка нет, это все
+* инициатива разработчиков компиляторов, поэтому не всегда ее есть возможность использовать.
+* Поэтому надо научиться писать и обычные include guards */
 #pragma once
 void hello_world_output();
