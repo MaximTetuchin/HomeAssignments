33660b2e282653adda6192d55bbbc2a92fce9d88
Andrey Isakov <73563875+AndreyIsakov01@users.noreply.github.com>
2024-09-29 07:04:40 -0700

    Comments for function_header.hpp

--- Assignment1/function_header.hpp
+++ Assignment1/function_header.hpp
@@ -1,8 +1,11 @@
 /* Maxim Tetuchin tetuhin@inbox.ru | st128993@student.spbu.ru
 Hello_world project
 */
+// Поменять название файла, чтобы было такое же как и у файла, в котором определяется функция
+// https://guiquanz.gitbooks.io/google-cc-style-guide/content/ebook/Naming.html
+
 #include <string>
-#ifndef Flag
+#ifndef Flag  // Поменять название guard'а. https://guiquanz.gitbooks.io/google-cc-style-guide/content/ebook/Header_Files.html
 #define Flag
 
 void hello_world_output(std::string word);
