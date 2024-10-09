ea32fe4483b8fcdea05b8352286187ed7d442cf2
MaximTetuchin <st128993@student.spbu.ru>
2024-10-09 20:47:16 +0300

    2b is done

--- Assignment2/b/functions_declaration.hpp
+++
@@ -1,3 +0,0 @@
-/* Инпуты */
-#include <iostream>
-#include <string>
--- Assignment2/b/main.cpp
+++ Assignment2/b/main.cpp
@@ -1,5 +1,57 @@
-
-/* Инпуты */
-#include "functions_declaration.hpp"
-
 /* Код */
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main(){
+    /* Есть 2 способа решить поставленную задачу
+    1) С помощью 2 массивов. Но тогда придется каждый раз копировать эллементы одного массива в другой,
+    что долго
+    2) Задать динамический массив длинной в n элементов, тогда и двигать указатель
+    */
+    double* stack = new double[100];
+    double* j = stack; // указатель на начало массива
+
+    std::cout<<"Введите обратную польскую запись\n";
+    std::string word; // Переменная для хранения конкретного символа
+    /* Создаем поток*/
+    std::string userInput;
+    std::getline(std::cin,userInput);
+    std::stringstream stream(userInput);
+
+    /*Пишем условие*/
+    while (stream>>word){
+        if (word=="*" || word=="+" || word == "-" || word == "/"){
+            if (word=="*"){
+                double element1 = *j;
+                j--;
+                double element2 = *j;
+                *j = element1*element2;
+            }
+            if (word=="+"){
+                double element1 = *j;
+                j--;
+                double element2 = *j;
+                *j = element1+element2;
+            }
+            if (word=="-"){
+                double element1 = *j;
+                j--;
+                double element2 = *j;
+                *j = element2-element1;
+            }
+            if (word=="/"){
+                double element1 = *j;
+                j--;
+                double element2 = *j;
+                *j = element2/element1;
+            }
+        } else {
+            j++;
+            int str_to_int = stol(word);
+            *j = str_to_int;
+        }
+    }
+    std::cout<<*j<<"\n";
+    delete[] stack;
+}
