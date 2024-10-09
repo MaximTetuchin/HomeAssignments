51d09d75f80979ca644746d0345d95a1235e15e1
MaximTetuchin <st128993@student.spbu.ru>
2024-10-09 20:46:19 +0300

    2b is done

--- Assignment2/a/main.cpp
+++ Assignment2/a/main.cpp
@@ -1,11 +1,9 @@
 #include "functions_declaration.hpp"
 
 int main(){
-    {
-        std::string filePath;
-        std::cout<<"Сейчас Вы можете указать относительный путь файла для проверки\n";
-        std::cout<<"Если же Вы введете в консоль '-',то будет отреверсирован файл putTextHere.txt\n";
-        std::cin >> filePath;
-        TextReverse(filePath);
-    }
+    std::string filePath;
+    std::cout<<"Сейчас Вы можете указать относительный путь файла для проверки\n";
+    std::cout<<"Если же Вы введете в консоль '-',то будет отреверсирован файл putTextHere.txt\n";
+    std::cin >> filePath;
+    TextReverse(filePath);
 }
