25269682c29a2dc56168766eea59a2bb5f1a2202
MaximTetuchin <st128993@student.spbu.ru>
2024-12-04 21:27:16 +0300

    added one more test

--- Assignment5/virtual_methods_test.cpp
+++ Assignment5/virtual_methods_test.cpp
@@ -39,6 +39,50 @@ TEST(TransformerVirtualMethods, PolymorphicTest) {
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_NE(output.find("Transformer casts an all-consuming black hole"), std::string::npos);
 }
+
+#include <gtest/gtest.h>
+#include <vector>
+#include "Transformer.h"
+#include "Soldiers.h"
+#include "Scouts.h"
+#include "Artillery.h"
+
+// Vector test for virtual methods
+TEST(TransformerVirtualMethods, VectorTest) {
+    std::vector<Transformer*> transformers;
+    transformers.push_back(new Artillery("Enigma", 100));
+    transformers.push_back(new Artillery("Huskar", 200));
+    transformers.push_back(new Artillery("Invoker", 300));
+
+    transformers.push_back(new Soldiers("Pudge", 100));
+    transformers.push_back(new Soldiers("Juggernaut", 200));
+    transformers.push_back(new Soldiers("Axe", 300));
+
+    transformers.push_back(new Scouts("Phantom Assassin", 100));
+    transformers.push_back(new Scouts("Shadow Fiend", 200));
+    transformers.push_back(new Scouts("Earthshaker", 300));
+
+    for (size_t i = 0; i < transformers.size(); ++i) {
+        // Test ultimative() method
+        testing::internal::CaptureStdout();
+        transformers[i]->ultimative();
+        std::string ultimativeOutput = testing::internal::GetCapturedStdout();
+        EXPECT_NE(ultimativeOutput.find("Transformer casts an all-consuming black hole ???"), std::string::npos);
+
+        // Test damaged() method
+        testing::internal::CaptureStdout();
+        transformers[i]->damaged();
+        std::string damagedOutput = testing::internal::GetCapturedStdout();
+        EXPECT_NE(damagedOutput.find("Ai,blin, ya masliny poimal!"), std::string::npos);
+
+        // Test scream() method
+        testing::internal::CaptureStdout();
+        transformers[i]->scream();
+        std::string screamOutput = testing::internal::GetCapturedStdout();
+        EXPECT_NE(screamOutput.find("You hear a very loud scream!!!"), std::string::npos);
+    }
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
